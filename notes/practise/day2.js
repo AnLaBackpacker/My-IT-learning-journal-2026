5755e64c08242b36ec8804d7675cd52fc8eaadc1
Quang Minh Nguyen <anla01.contact@gmail.com>
2026-05-02 08:19:05 +0700

    Update day2.js with Day 3 learning notes
    Added Day 3 updates and additional learning notes.

--- notes/practise/day2.js
+++ notes/practise/day2.js
@@ -2,3 +2,7 @@ console.log("Day 2 - Learning Javascrpt");
 console.log("Still improving step by step");
 
 // TODO: learn Javascript funtions more deeply
+console.log("Day 3 update");
+console.log("Learning continues");
+
+// updated file for practise
